8365504dfcae3602022b453d0f7c6ac94723b7a6
Hosting IDC Cloud <hostidc@mail85.com>
2026-05-03 16:37:08 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
+  skipMiddlewareUrlNormalize: true,
+  skipTrailingSlashRedirect: true,
   experimental: {
     // This is needed for standalone output to work correctly
     outputFileTracingRoot: undefined,
-    outputStandalone: true,
-    skipMiddlewareUrlNormalize: true,
-    skipTrailingSlashRedirect: true,
   },
 };
