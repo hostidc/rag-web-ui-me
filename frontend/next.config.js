998d5c08d76a5fe12bd09fae6f81a83776e65c40
Hosting IDC Cloud <hostidc@mail85.com>
2026-05-03 16:41:24 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,17 @@ module.exports = {
   output: "standalone",
   skipMiddlewareUrlNormalize: true,
   skipTrailingSlashRedirect: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     // This is needed for standalone output to work correctly
     outputFileTracingRoot: undefined,
